6fdda115e078bc3616e5f6de4d584613884061d2
shyam kumar <kumarshyam5@gmail.com>
2015-10-14 16:41:53 -0500

    Added New Controller and modified app.js file

---
+++ www/js/Controllers.js
@@ -0,0 +1,18 @@
+﻿
+angular.module('starter.Controllers', [])
+.controller('TodoListController', function ($scope) {
+
+    $scope.DriverName = "John Smith";
+    $scope.DriverId = 3456;
+
+    $scope.TodoList = [{
+        ETD: 'Current ETD',
+        Date: '10/24/2015 14:00'
+    }, {
+        ETD: 'Next ETD',
+        Date: '10/25/2015 11:00'
+    }, {
+        ETD: 'Next ETD',
+        Date: '10/25/2015 18:00'
+    }]
+});
