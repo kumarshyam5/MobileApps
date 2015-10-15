c9a2b835d7e3f8307296398c038fe40ba3fc9106
Nimil Peethambaran <nimil.p@hotmail.com>
2015-10-14 22:48:32 -0500

    removed redundant usage of controllers

--- www/js/Controllers.js
+++ www/js/Controllers.js
@@ -18,10 +18,19 @@ angular.module('starter.Controllers', [])
     }]
 
     $scope.etaDetails = [{
-        CITY:'test'
+        tripId:'63455432',
+        location:'DC 6094, Bentonville',
+        eta:'10/25/2015 18:00'
     },
     {
-        CITY:'BLA'
+        tripId:'63455432',
+        location:'STORE 100, Bentonville',
+        eta:'10/25/2015 18:30'
+    },
+                        {
+        tripId:'63455432',
+        location:'STORE 1, Rogers',
+        eta:'10/25/2015 19:00'
     }]
 })
 .controller('NavController',function($scope,$stateParams){
