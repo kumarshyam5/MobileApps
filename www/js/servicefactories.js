75ef86ae9ca0bf99f2918d7d25b3cdca80b4cd31
shyam kumar <kumarshyam5@gmail.com>
2015-10-23 15:22:04 -0500

    Modified Update Eta method

--- www/js/servicefactories.js
+++ www/js/servicefactories.js
@@ -1,51 +1,55 @@
 
 angular.module('starter.ServiceFactory', [])
-.factory('etaServices',['$http',function($http){
+.factory('etaServices', ['$http', function ($http) {
+
+    //var baseUrl = 'http://driverdetailsdemoservice.cloudapp.net/DriverETADetails.svc';
     
-    var baseUrl = 'http://driverdetailsdemoservice.cloudapp.net/DriverETADetails.svc';
-    
+
+    var baseUrl= 'http://localhost:58841/DriverETADetails.svc';
+
     var driverID;
     ////TODO should be remove once itegrated to service
-    if(ionic.Platform.isAndroid()){
+    if (ionic.Platform.isAndroid()) {
         driverID = '3456';
-    }else{
+    } else {
         driverID = '8756';
     }
-    
-    
+
+
     var etaServices = {};
-    
+
     //return me the list of etas
-    etaServices.getEtaList = function () {       
-//        return $http.get(baseUrl + '/drivereta/'+driverID);  
+    etaServices.getEtaList = function () {
+        //        return $http.get(baseUrl + '/drivereta/'+driverID);  
         return $http({
-            url : baseUrl+'/drivereta/'+driverID,
+            url: baseUrl + '/drivereta/' + driverID,
             method: 'GET'
         });
     };
 
     etaServices.gerDriver = function () {
-//        return $http.get(baseUrl + '/driver/' + driverID);   
+        //        return $http.get(baseUrl + '/driver/' + driverID);   
         return $http({
-            url: baseUrl+'/driver/'+driverID,
-            method:'GET'
+            url: baseUrl + '/driver/' + driverID,
+            method: 'GET'
         });
     };
-    
+
     //return me a single eta
-    etaServices.getEta = function (id) {       
+    etaServices.getEta = function (id) {
         return $http.get(baseUrl + '/eta/' + id)
         .then(function (response) {
             return response.data;
-        })};
-    
-    //update an eta
-    etaServices.updateEta = function(eta){
-        return $http.put(baseUrl + '/' + eta)
-        .then(function(response)
-        {
+        })
+    };
+
+    // update an eta
+    etaServices.updateEta = function (eta) {
+        return $http.put(baseUrl + '/driverETAUpdate',eta)
+        .then(function (response) {
             return response.data;
-        })};
-    
+        });
+    };
+
     return etaServices;
 }]);
